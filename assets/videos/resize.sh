5f922c938dd86a9f58ea69b8cba3f4995927dbff
Agnes Köhler <agnes.koehler@codecentric.de>
2020-03-06 20:52:21 +0100

    Refine script to resize new videos

---
+++ assets/videos/resize.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+size=300
+for file in $(ls ./original); do
+  echo $file
+  ffmpeg -i ./original/${file} -vcodec mpeg4 -vf "crop=min(iw\,ih):min(iw\,ih), scale=${size}:${size}" -an ./${size}px/${file%.*}.mp4
+done
